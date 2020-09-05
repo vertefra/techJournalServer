400c86426703cd9655f7013cce61cc1cefaeb2d0
Maha Mahmood <>
2020-09-05 17:38:04 -0400

    created entry and user models

---
+++ models/entry.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const EntrySchema = new Schema(
+    {
+        entries: [{
+            title: { type: String, required: true },
+            content: {type: String, required: true },
+        }],
+        user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
+    },
+    { timestamps: true }
+);
+
+const Entry = mongoose.model('Entry', EntrySchema);
+
+module.exports = Entry;
