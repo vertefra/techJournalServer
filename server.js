4b22906f873d95f146a9d78b5b624cdee827694e
Maha Mahmood <>
2020-09-04 21:48:05 -0400

    created server.js, installed dependencies needed, add node_modules to .gitignore, and / route works

---
+++ server.js
@@ -0,0 +1,16 @@
+/* Dependencies */
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+/* Controller */
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+/* Listener */
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
