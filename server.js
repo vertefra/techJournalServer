65781ed1a5451d4798cc6f9be0c505f5cb695515
vertefra <verte.fra@gmail.com>
2020-09-05 14:21:49 -0400

    added controller in server.js

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 /* Dependencies */
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const mongoose = require('mongoose');
-const path = require('path');
-const cors = require('cors');
-const passport = require('./services/passport')();
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+const passport = require("./services/passport")();
 
 /* Database */
 const MONGODB_URI = process.env.MONGODB_URI;
 const db = mongoose.connection;
 
 mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
-db.on('open', () => {
-    console.log('Mongo is Connected')
+db.on("open", () => {
+  console.log("Mongo is Connected");
 });
 
 /* Middleware */
@@ -25,12 +25,20 @@ app.use(cors());
 app.use(express.json());
 app.use(passport.initialize());
 
-/* Controller */
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+/* Controllers */
+
+// =========================================== //
+//    USER CONTROLLER => /users                //
+// =========================================== //
+//
+// controls creates, updates, delete for User
+// model. /user/signup creates & aauthenticate
+// a new user, /users/login verifies authentic.
+// credentials and returns the atuth user
+
+app.use("/users", require("./controllers/users"));
 
 /* Listener */
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
