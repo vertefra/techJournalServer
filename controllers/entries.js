385e541a54a3c4dfe025ef945d0ab9d757ffccc1
vertefra <verte.fra@gmail.com>
2020-09-05 16:21:40 -0400

    get one entry

--- controllers/entries.js
+++ controllers/entries.js
@@ -12,14 +12,32 @@ router.get("/:id", (req, res) => {
 //    Get all entries => /users/:id/entries GET             //
 // ======================================================== //
 //
-// Get all the entries for a user with id === :id.
-// Id from user is taken from the url string with
-// returnParams function. For more info check
-// returnParams in services/utils.js
+// Get all the entries for a user with id users/:id.
+// usre_id is taken from the request with returnParams()
+// function. For more info check returnParams()
+// in services/utils.js
 
 router.get("/", (req, res) => {
-  const id = returnParams(req)[0];
-  console.log(id);
-  User.findById(id);
+  const user_id = returnParams(req)[0];
+  Entry.findOne({ user_id }, (error, entry) => {
+    entry
+      ? res.status(200).json({ entries: entry.entries })
+      : res.status(404).json({ error: "Entry not found: " + error });
+  });
 });
+
+// ======================================================== //
+//    Get one entry => /users/:id/entries/:id               //
+// ======================================================== //
+//
+// Get the entries with id entry/:id for a user with id
+// users/:id. user_id and entry_id are taken from the
+// request with returnParams() function. For more info check
+// returnParams in services/utils.js
+
+router.get("/:id", (req, res) => {
+  const [user_id, entry_id] = returnParams(req);
+  Entry.findOne({ user_id });
+});
+
 module.exports = router;
