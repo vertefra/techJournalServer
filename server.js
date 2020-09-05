7205057fa111fb7d5e9a80e25be4c5e7386a5998
Maha Mahmood <>
2020-09-05 11:11:11 -0400

    connected to mongo

--- server.js
+++ server.js
@@ -3,22 +3,22 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 // const path = require('path');
 // const cors = require('cors');
 // const passport = require('./config/passport')();
 
 /* Database */
-// const MONGODB_URI = process.env.MONGODB_URI;
-// const db = mongoose.connection;
+const MONGODB_URI = process.env.MONGODB_URI;
+const db = mongoose.connection;
 
-// mongoose.connect(MONGODB_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// });
-// db.on('open', () => {
-//     console.log('Mongo is Connected')
-// });
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+db.on('open', () => {
+    console.log('Mongo is Connected')
+});
 
 /* Middleware */
 // app.use(cors());
