fb141ea905260dd43795000bc4af4e95ee32929d
vertefra <verte.fra@gmail.com>
2020-09-06 07:23:10 -0400

    fixing controllers based on schemas

--- models/entry.js
+++ models/entry.js
@@ -1,17 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const EntrySchema = new Schema(
-    {
-        entries: [{
-            title: { type: String, required: true },
-            content: {type: String, required: true },
-        }],
-        user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
-    },
-    { timestamps: true }
+  {
+    entries: [
+      {
+        title: { type: String, required: true },
+        content: { type: String, required: true },
+      },
+    ],
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
 );
 
-const Entry = mongoose.model('Entry', EntrySchema);
+const Entry = mongoose.model("Entry", EntrySchema);
 
 module.exports = Entry;
